3b88ed2cfbc6c660e8a411cc9fefddc975e615c9
Solomon Egwuonwu <segwuonwu@gmail.com>
2019-02-27 02:19:18 -0800

    Day 1 of harkerrank challenge

---
+++ Day1/src/solution/Solution.java
@@ -0,0 +1,36 @@
+package solution;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+
+        /* Declare second integer, double, and String variables. */
+        int myInt = scan.nextInt();
+        double myDouble = scan.nextDouble();
+        String myString = scan.next();
+        myString += scan.nextLine();
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i+myInt);
+        /* Print the sum of the double variables on a new line. */
+		System.out.println(d+myDouble);
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+        System.out.println(s + myString);
+
+        scan.close();
+    }
+}
